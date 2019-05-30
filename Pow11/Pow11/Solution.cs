d9367497c664bb3cc4089054d05ed5634dca7731
John Burkert <bj.59191@konamigaming.com>
2019-05-30 14:07:53 -0700

    some codility tests

---
+++ Pow11/Pow11/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Numerics;
+
+class Solution
+{
+    private static BigInteger Pow(int i, int n)
+    {
+        if (n == 0)
+            return 1;
+        var result = new BigInteger(i);
+        while (--n > 0)
+            result *= i;
+        return result;
+    }
+
+    public int solution(int N)
+    {
+        return Pow(11, N).ToString().Count(x => x == '1');
+    }
+}
---
+++ Times17DecDigitsSum/Times17DecDigitsSum/Solution.cs
@@ -0,0 +1,24 @@
+﻿
+class Solution
+{
+    public int solution(int[] A)
+    {
+        var total = 0;
+        var prev = 0;
+        
+        foreach (var i in A)
+        {
+            var curr = i * 17 + prev;
+            total += curr % 10;
+            prev = curr / 10;
+        }
+
+        while (prev > 0)
+        {
+            total += prev % 10;
+            prev /= 10;
+        }
+
+        return total;
+    }
+}
