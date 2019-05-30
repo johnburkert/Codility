ebc9af6a63a14b0980aaf2d19c6c3c3c20a34617
John Burkert <bj.59191@konamigaming.com>
2019-05-30 14:20:37 -0700

    added dice rotations

---
+++ DiceRotations/DiceRotations/Solution.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+class Solution
+{
+    private static int Moves(int a, int b)
+    {
+        // if equal then zero, if sum 7 then 2 else 1
+        return (a == b) ? 0 : (a + b == 7) ? 2 : 1;
+    }
+    
+    public int solution(int[] A)
+    {
+        return Enumerable.Range(1, 6).Select(x => A.Select(y => Moves(x, y)).Sum()).Min();
+    }
+}
